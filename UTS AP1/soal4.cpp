e672a8db2a7ad8d8e8b6ef8f1fa21db0bab40979
nauflzuhd <nauflzuhd@gmail.com>
2025-04-19 10:34:23 +0700

    MuhammadNaufalZuhdi_241401052__UTS_AP1

--- UTS AP1/soal4.cpp
+++ UTS AP1/soal4.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 int main () {
     string mantra;
-    int jumlah = 0;
+    int jumlah = 0, i = 0;
     cout << "Mantra: ";
     cin >> mantra;
-    for (int i = 0; i <= mantra.length(); i++) {
+    while (i <= mantra.length()) {
         if (mantra[i] == 'a' || mantra[i] == 'A' || mantra[i] == 'i' || mantra[i] == 'I' || mantra[i] == 'u' || mantra[i] == 'U' || mantra[i] == 'e' || mantra[i] == 'E' || mantra[i] == 'o' || mantra[i] == 'O') {
             jumlah += 1;
         }
+        i++;
     }
     (jumlah > 0) ? cout << "Kekuatan mantra: " << jumlah << " vokal" : cout << "Mantra tidak valid! Tidak mengandung vokal.";
     return 0;
